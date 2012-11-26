2d0d6064369c9eeb38813fbe850893caeec42aef
fz1989 <fz1989fz@gmail.com>
2012-11-26 13:32:29 +0800

    建立工程

---
+++ function.php
@@ -0,0 +1,55 @@
+﻿<?php
+$con = mysql_connect("localhost","root","fz1989");
+mysql_select_db("p2p", $con);
+function doRegist()
+{
+	$username = isset($_REQUEST["username"]) ? $_REQUEST["username"] : "";
+	$password = isset($_REQUEST["password"]) ? $_REQUEST["password"] : "";
+	$password = $password . "nimeide"; 
+	$password = md5($password, FALSE);
+	if ($username == "" || $password == "") {
+		return "NULL";
+	}
+	$sql = "select username from userinfo where username = '$username'";
+	$result = mysql_query($sql);
+	if (mysql_fetch_array($result)) {
+		return "EXISI";
+	}
+	$sql = "insert into userinfo (username, password) values ('$username', '$password')";
+	if (!mysql_query($sql))
+	{
+		return die('Error: ' . mysql_error());
+	}
+	return "SUCCESS";
+}
+
+function doLogin() 
+{
+	echo "Login";
+}
+
+function doShareFile()
+{
+	echo "ShareFile";
+}
+
+function doCancleShareFile()
+{
+	echo "CancleShareFile";
+}
+
+function doDownload()
+{
+	echo "Download";
+}
+
+function doUpload()
+{
+	echo "Upload";
+}
+
+function doLogoff()
+{
+	echo "Logoff";
+}
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<?php
+header('Content-Type: text/html; charset=utf-8');
+require_once("function.php");
+if (!isset($_REQUEST["action"])) {
+	echo json_encode("Error");
+} else {
+	$action = $_REQUEST["action"];
+	echo $action;
+	if ($action == "regist") {
+		echo json_encode(doRegist());
+	} 
+	else if ($action == "login") {
+		echo "nimeia";
+		doLogin();
+	}
+	else if ($action == "sharefile") {
+		doSharefile();
+	}
+	else if ($action == "canclesharefile") {
+		doCanclesharefile();
+	}
+	else if ($action == "download") {
+		doDownload();
+	} 
+	else if ($action == "upload") {
+		doUpload();
+	}
+	else if ($action == "logoff") {
+		doLogoff();
+	}
+}
+?>
